979820fb8516895076d4359c2b2f9e54c781ed1a
Kirill <kireeffkirill21@yandex.ru>
2025-05-13 10:43:55 +0300

    test

--- src/main/java/ru/yandex/practicum/catsgram/model/Cat.java
+++ src/main/java/ru/yandex/practicum/catsgram/model/Cat.java
@@ -1,20 +1,20 @@
-package ru.yandex.practicum.catsgram.model;
-
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-public class Cat {
-    private String color;
-    private int age;
-
-    @Override
-    public String toString() {
-        return "Cat{" +
-                "color='" + color + '\'' +
-                ", age=" + age +
-                '}';
-    }
-}
+//package ru.yandex.practicum.catsgram.model;
+//
+//import lombok.Getter;
+//import lombok.Setter;
+//
+//@Getter
+//@Setter
+//public class Cat {
+//    private String color;
+//    private int age;
+//
+//    @Override
+//    public String toString() {
+//        return "Cat{" +
+//                "color='" + color + '\'' +
+//                ", age=" + age +
+//                '}';
+//    }
+//}
 
